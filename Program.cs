18781fe4a66c8a32ad73717506f6bed8d8fead25
Programar_2023 <danielamiranda.contato@gmail.com>
2023-02-22 22:40:52 -0300

    ajustes cors

--- Program.cs
+++ Program.cs
@@ -31,6 +31,13 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(x => x
+               .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+
+
+
 app.MapControllers();
 
 app.Run();
